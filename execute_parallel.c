6a48e0c8866ad2fa2873bd0dd17a0188fb3fc339
Chris Capistran <capistranc@gmail.com>
2015-11-09 00:00:20 -0800

    threading

--- execute_parallel.c
+++ execute_parallel.c
@@ -11,6 +11,7 @@
 #include <stdlib.h> // for exit
 #include <fcntl.h> // for file open constants
 
+#include <pthread.h>
 
 #include <error.h>
 
@@ -33,64 +34,113 @@ void find_dependency(int** dependency_graph, int* has_dependency, int num_cmds) 
 	printf("\n");*/
 }
 
+struct execute_wrapper_arg {
+  int cmd_idx;
+  int num_cmds;
+  int ** dependency_graph;
+  int *has_dependency;
+  command_t cmd;
+};
+
+void *execute_wrapper(void* arg) {
+  int cmd_idx = ((struct execute_wrapper_arg*)arg)->cmd_idx;
+  int num_cmds = ((struct execute_wrapper_arg*)arg)->num_cmds;
+  int ** dependency_graph = ((struct execute_wrapper_arg*)arg)->dependency_graph;
+  int *has_dependency = ((struct execute_wrapper_arg*)arg)->has_dependency;
+  command_t cmd = (command_t)((struct execute_wrapper_arg*)arg)->cmd);
+
+  execute_command(cmd);
+  //printf("dependcency graph\n");
+  int i;
+  int j;
+  for (i = 0; i < num_cmds; i++) {
+    for (j = 0; j < num_cmds; j++) {
+      if (i == cmd_idx || j == cmd_idx) {
+	dependency_graph[i][j] = 0;
+      }
+      //printf("%d", dependency_graph[i][j]);
+    }
+    //printf("\n");
+  }
+find_dependency(dependency_graph, has_dependency, num_cmds);
+}
+
 void run_ready_commands(command_t* forrest, int** dependency_graph, int* has_dependency, int* executable, int num_cmds)
 {
-	//run commands with no dependency that are executable, i.e. not already running or not completed
-	int i;
-	int j;
-	int cmd_idx;
-	for (cmd_idx = 0; cmd_idx<num_cmds; cmd_idx ++) {
-	  if(!has_dependency[cmd_idx] && executable[cmd_idx]) {
-			pid_t pid = fork();
-			if (pid == 0) {
-			  //make this command non-executable because we are about to run it
-			  printf("execute command %d\n", cmd_idx);
-			  executable[cmd_idx] = 0;
-			  execute_command(forrest[cmd_idx]);
-			  //finished executing command so we update the dependency graph to remove any dependencies on it
-			  //printf("dependcency graph\n");
-			  for (i = 0; i < num_cmds; i++) {
-			    for (j = 0; j < num_cmds; j++) {
-			      if (i == cmd_idx || j == cmd_idx) {
-				dependency_graph[i][j] = 0;
-			      }
-			      //printf("%d", dependency_graph[i][j]);
-			    }
-			    //printf("\n");
-			    }
-			  //now we update our dependency indicator using our updated dependency graph
-			  find_dependency(dependency_graph, has_dependency, num_cmds);
-			  //finally we call this function recursively to execute any executable commands who have had their dependencies resolved
-			  run_ready_commands(forrest, dependency_graph, has_dependency, executable, num_cmds);
-			  //while (1) {}
-			  //printf("child thread exit\n");
-			  return;
-			  //break;//_exit(1);
-			} else {
-			  continue; //parent so continue making children
-			}
+  //run commands with no dependency that are executable, i.e. not already running or not completed
+  int i;
+  int j;
+  int cmd_idx;
+  int any_executable = 0;
+  while(1) {
+    for(cmd_idx = 0; cmd_idx <num_cmds; cmd_idx ++) {
+      if(executable[cmd_idx] == 1) {
+	any_executable = 1;
+      }
+      if(any_executable == 0) {return;}
+      for (cmd_idx = 0; cmd_idx<num_cmds; cmd_idx++) {
+	if(!has_dependency[cmd_idx] && executable[cmd_idx]) {
+	  executable[cmd_idx] = 0;
+	  pthread_t idk;
+	  struct execute_wrapper_arg* arg = (struct execute_wrapper_arg*)malloc(sizeof(struct execute_wrapper_arg));
+	  arg->cmd_idx = cmd_idx;
+	  arg->num_cmds = num_cmds;
+	  arg->dependency_graph = dependency_graph;
+	  arg->has_dependency = has_dependency;
+	  arg->cmd = forrest[cmd_idx];
+ 
+	  pthread_create(&idk, NULL, execute_wrapper, (void*)arg);
+	  /*pid_t pid = fork();
+	  if (pid == 0) {
+	    //make this command non-executable because we are about to run it
+	    printf("execute command %d\n", cmd_idx);
+	    executable[cmd_idx] = 0;
+	    execute_command(forrest[cmd_idx]);
+	    //finished executing command so we update the dependency graph to remove any dependencies on it
+	    //printf("dependcency graph\n");
+	    for (i = 0; i < num_cmds; i++) {
+	      for (j = 0; j < num_cmds; j++) {
+		if (i == cmd_idx || j == cmd_idx) {
+		  dependency_graph[i][j] = 0;
 		}
-		//printf("parent thread continue\n");
+		//printf("%d", dependency_graph[i][j]);
+	      }
+	      //printf("\n");
+	    }
+	    //now we update our dependency indicator using our updated dependency graph
+	    find_dependency(dependency_graph, has_dependency, num_cmds);
+	    //finally we call this function recursively to execute any executable commands who have had their dependencies resolved
+	    run_ready_commands(forrest, dependency_graph, has_dependency, executable, num_cmds);
+	    //while (1) {}
+	    //printf("child thread exit\n");
+	    return;
+	    //break;//_exit(1);
+	  } else {
+	  continue; //parent so continue making children*/
 	}
-	//printf("parent function exit\n");
-	//while (1);
+      }
+	//printf("parent thread continue\n");
+    }
+  //printf("parent function exit\n");
+  //while (1);
+  }
 }
 
 void executeTimeTravel(command_t* forrest, int** dependency_graph, int num_cmds)
 {
   printf("parallel command execution\n");
   int cmd_idx;
-	printf("num_cmds: %d\n", num_cmds);
-	int* has_dependency = (int*)malloc(sizeof(int)*num_cmds);
-	int* executable = (int*)malloc(sizeof(int)*num_cmds);
-        for (cmd_idx = 0; cmd_idx < num_cmds; cmd_idx++) {
-	  executable[cmd_idx] = 1;
-	  printf("execute command %d\n", cmd_idx);
-	  execute_command(forrest[cmd_idx]);
-	}
-	printf("PARRALELLIZED VERIOSN\n");
-	find_dependency(dependency_graph, has_dependency, num_cmds);
-	run_ready_commands(forrest, dependency_graph, has_dependency, executable, num_cmds);
+  printf("num_cmds: %d\n", num_cmds);
+  int* has_dependency = (int*)malloc(sizeof(int)*num_cmds);
+  int* executable = (int*)malloc(sizeof(int)*num_cmds);
+  for (cmd_idx = 0; cmd_idx < num_cmds; cmd_idx++) {
+    executable[cmd_idx] = 1;
+    printf("execute command %d\n", cmd_idx);
+    execute_command(forrest[cmd_idx]);
+  }
+  printf("PARRALELLIZED VERIOSN\n");
+  find_dependency(dependency_graph, has_dependency, num_cmds);
+  run_ready_commands(forrest, dependency_graph, has_dependency, executable, num_cmds);
 }
 
 	
